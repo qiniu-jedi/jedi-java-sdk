196d02cd200924f92b1ef839f41bfdf0b3cb8264
Anbia <bingtianbaihua@users.noreply.github.com>
2016-11-12 11:46:13 +0800

    bug fix

--- src/main/java/com/qiniu/Const.java
+++ src/main/java/com/qiniu/Const.java
@@ -6,7 +6,7 @@ package com.qiniu;
 public class Const {
 
     public static final String SDK_VERSION = "1.0.0";
-    public static final String SDK_USER_AGENT = "jedi-sdk-go";
+    public static final String SDK_USER_AGENT = "jedi-sdk-java";
     public static final String DEFAULT_API_VERSION = "v1";
 
     public static final String HOST = "http://jedi.qiniuapi.com";
